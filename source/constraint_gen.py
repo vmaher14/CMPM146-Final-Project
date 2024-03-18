d342acb58c7733167fa9caf1be82fc818eb0c2a4
Andrew <andrewjsusanto@gmail.com>
2024-03-18 16:25:49 -0700

    domain gen

--- source/constraint_gen.py
+++ source/constraint_gen.py
@@ -6,6 +6,13 @@ import itertools
 
 def generateMonsters(LevelObject):
     mobs = creatures.PossibleMobs(LevelObject.depth)
-    domain = itertools.combinations(mobs)
+    domain = itertools.combinations_with_replacement(mobs, 3)
+    print(domain)
+    # find possible mobs
+    # generate domain of possible mobs
+    
 
+mobtest = [None, "Rat", "Wolf"]
+domain = list(itertools.combinations_with_replacement(mobtest, 3))
+print(domain)
     
